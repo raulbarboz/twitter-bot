bc751548769493e1948f42857c48acc3eec5276f
Raul Barboza <ruzito@gmail.com>
2018-04-18 22:08:17 +0000

    all done

--- twitter.js
+++ twitter.js
@@ -2,6 +2,7 @@ const Twit = require('twit');
 const config = require('./config.js');
 let schedule = require('node-schedule');
 const request = require('request');
+const translate = require('google-translate-api');
 
 let T = new Twit(config);
 
@@ -30,10 +31,24 @@ var j = schedule.scheduleJob({hour: 19, minute: 20}, function(){
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', JSON.parse(body).quote); // Print the HTML for the Google homepage.
-    tweetIt('Auto post das 4:20: '+ JSON.parse(body).quote)
+    
+    translate(JSON.parse(body).quote, {from: 'en', to: 'pt'}).then(res => {
+        console.log(res.text);
+        //=> Ik spreek Nederlands!
+        //console.log(res.from.text.autoCorrected);
+        //=> true
+        //console.log(res.from.text.value);
+        //=> I [speak] Dutch!
+        //console.log(res.from.text.didYouMean);
+        //=> false
+        tweetIt('Twit automático das 4:20: '+ res.text)
+    }).catch(err => {
+        //console.error(err);
+    });
+
+    //tweetIt('Auto post das 4:20: '+ JSON.parse(body).quote)
   });
   
 });
-
 
 
